7fe5484e3148a98219d622802b7e080f19c8011e
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2020-12-05 14:33:41 +0100

    Navbar hidden in scrolling added and modal added form competences

--- src/Competences.js
+++ src/Competences.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import Modal from './Modal';
+import React, { useState, useEffect } from 'react';
 import html from './images/html.png';
 import aws from './images/aws.png';
 import c from './images/c.png';
@@ -13,16 +12,61 @@ import reactjs from './images/react.png';
 import bug from './images/bug.png';
 import node from './images/node.png';
 
-
 function Cards({img, name}) {
 	const [stateHere, setStateHere] = useState(false);
-	const handleClick = () => {
-		console.log("clicked");
-		setStateHere(true);
-	}
 	return (
 		<>
-	<div className="h-40 w-40 relative cursor-pointer mb-2" onClick={handleClick}>
+		{stateHere ? (
+			<>
+<div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            onClick={() => setStateHere(false)}
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                  <h3 className="text-3xl font-semibold text-black">
+                    {name}
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setStateHere(false)}
+                  >
+                    <span className="text-black">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-gray-600 text-lg leading-relaxed">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    style={{ transition: "all .15s ease" }}
+                    onClick={() => setStateHere(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+	  ) : null}
+	<div className="h-40 w-40 relative cursor-pointer mb-2" onClick={() => setStateHere(true)}>
 			<div className="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl"></div>
 			<div className="absolute inset-0 transform  hover:scale-75 transition duration-300 p-2">
 			<div className="h-full w-full bg-white rounded-lg shadow-1xl">
@@ -31,7 +75,6 @@ function Cards({img, name}) {
 			</div>
 			</div>
 		</div>
-		{stateHere ? <Modal name={name} state={stateHere}/> : null}
 		</>
 	)
 }
@@ -53,19 +96,17 @@ function Competences() {
 				"Node Js" ],
 		image: [html, aws, c, docker, git, javascript, kubernetes, network, windows, reactjs, bug, node]
 	})
-
 	return (
 		<>
 	<section className="bg-white border-b py-8">
       <div className="container max-w-5xl mx-auto m-8">
-        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800" id="competences">
 		Competences
         </h1>
         <div className="w-full mb-4">
           <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
 		 <div className="flex flex-wrap">
-
 			{cardName.name.map((e, i) => <Cards key={i} img={cardName.image[i]} name={e}/>)}
         	</div>
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
 function Navbar() {
+  const name = 'Amine TAHIRI';
+  const [navbar, setNavbar] = useState(true);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  })
+  const handleScroll = () => {
+    if (window.scrollY >= 50){
+      setNavbar(false);
+    }else setNavbar (true);
+  }
 	return (
-		<nav id="header" className="fixed w-full z-30 top-0 text-white">
+    <>
+{	navbar ?	<nav id="header" className="fixed w-full z-30 top-0 text-white">
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
           <div className="pl-4 flex items-center">
             <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/">
               {/*Icon from: http://www.potlabicons.com/ */}
-              Amine TAHIRI
+              {name}
             </a>
           </div>
           <div className="block lg:hidden pr-4">
@@ -20,13 +32,13 @@ function Navbar() {
           <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
             <ul className="list-reset lg:flex justify-end flex-1 items-center">
               <li className="mr-3">
-              <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#education">Education</a>
+              <a className="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#education">Education</a>
               </li>
               <li className="mr-3">
-                <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="/">Competences</a>
+                <a className="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#competences">Competences</a>
               </li>
               <li className="mr-3">
-                <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="/">Projects</a>
+                <a className="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="/">Projects</a>
               </li>
             </ul>
             <button id="navAction" className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
@@ -35,7 +47,8 @@ function Navbar() {
           </div>
         </div>
         <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
-      </nav>
+      </nav> : null}
+      </>
 	)
 }
 
