875713660f60598db57dc8a8543f36f13cc5742c
Amine Tahiri <atahiri@e2r10p2.1337.ma>
2020-12-11 12:32:54 +0100

    adding navbar media dropdown and fixing some key errors

--- src/Education.js
+++ src/Education.js
@@ -13,7 +13,6 @@ function Education({education}) {
           </div>
       {Object.keys(education).map(function (key , i) {
         return (
-        <>
           <div className="flex flex-wrap" key={i}>
             <div className="w-5/6 sm:w-1/2 p-6">
               <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
@@ -28,11 +27,11 @@ function Education({education}) {
             <div className="w-full sm:w-1/2 p-6">
               <svg className="w-full sm:h-64 mx-auto" viewBox="0 0 1177 598.5" xmlns="http://www.w3.org/2000/svg">
                 <title>education 1</title>
-                <PathEducation />
+                <PathEducation key={i}/>
               </svg>
             </div>
           </div>
-          </>)}
+          )}
       )}
            </div>
          </section>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
+import "./assets/navbar.css";
 
 function Navbar({fullname}) {
   const [navbar, setNavbar] = useState(true);
+  const [navbarOpen, setNavbarOpen] = useState(false);
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
   })
@@ -16,19 +18,18 @@ function Navbar({fullname}) {
         <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
           <div className="pl-4 flex items-center">
             <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/">
-              {/*Icon from: http://www.potlabicons.com/ */}
               {fullname}
             </a>
           </div>
           <div className="block lg:hidden pr-4">
-            <button id="nav-toggle" className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+            <button onClick={() => {setNavbarOpen(!navbarOpen)}} id="nav-toggle" className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
               <svg className="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <title>Menu</title>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
               </svg>
             </button>
           </div>
-          <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
+          <div className={"w-full flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0 sm:text-center sm:bg-opacity-50 bg-white lg:bg-transparent p-4 lg:p-0 z-20" + (navbarOpen ? " bg-gradient-to-r navbar-gradient to-navbar-end" : " hidden")} id="nav-content">
             <ul className="list-reset lg:flex justify-end flex-1 items-center">
               <li className="mr-3">
               <a className="inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#education">Education</a>
